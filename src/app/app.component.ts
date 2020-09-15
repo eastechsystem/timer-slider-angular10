fb24aec0c91084727cd4ab04e85f95d161601eb4
Jahanzaib.Ali <jahanzaib.ali@MacBook-Pro.fritz.box>
2020-09-15 14:55:18 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, NgModule, enableProdMode, OnInit } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import {DxButtonModule} from 'devextreme-angular';
+import notify from 'devextreme/ui/notify';
+import * as $ from 'jquery';
+
+if(!/localhost/.test(document.location.host)) {
+    enableProdMode();
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'timer-slider-angular10';
+  
+  startValue: Date = new Date(2012, 8, 29, 0, 0, 0);
+  endValue: Date = new Date(2012, 8, 29, 24, 0, 0);
+  startSelectedValue: Date = new Date(2012, 8, 29, 11, 0, 0);
+  endSelectedValue: Date = new Date(2012, 8, 29, 11, 0, 0);
+  
+
+  constructor() { }
+
+  ngOnInit() {
+    $(document).ready(function() { 
+      $("#range-selector").addClass("container flex");
+      
+      var x,y;
+      var mouseDown = false;
+      $('.slider-marker').mousedown(function(e) {
+        x = e.pageX;
+        y = e.pageY;
+        mouseDown = true;
+        console.log( "y = " + y + ", x = " + x);
+
+        if (mouseDown) {
+          mouseDown = false;
+          e.preventDefault();
+          $('.slider-marker').mouseup(function(e){
+            if (x > 700) {
+              // $('#range-selector').animate({scrollLeft:'+=10%'}, 1000, "swing").delay( 800 ).fadeIn( 500 ); 
+              // $('#range-selector').animate({scrollLeft:'+=100%'});
+              $('#range-selector').animate({scrollLeft:'+=100%'}, 500);
+            } else if (x < 500) {
+              // $('#range-selector').animate({scrollLeft:'-=10%'}, 1000, "swing").delay( 800 ).fadeIn( 500 ); 
+              // $('#range-selector').animate({scrollLeft:'-=100%'});
+              $('#range-selector').animate({scrollLeft:'-=100%'}, 500);
+            }
+  
+          });
+        }
+      });
+      // Method ends
+
+    });
+  }
+
+  okClicked (e) {
+    $(".dxrs-slidersContainer .slider").css("display", 'block');
+    $(".dx-button-mode-contained").css("pointer-events", 'none');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
+import { DxRangeSelectorModule, DxButtonModule } from 'devextreme-angular';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DxRangeSelectorModule,
+    DxButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+platformBrowserDynamic().bootstrapModule(AppModule);
